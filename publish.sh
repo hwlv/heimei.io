65df44250a0304a9e68bb851b4f8ca5b57e2ff4c
hongwang.lv <hongwang.lv@weimob.com>
2022-06-24 14:01:14 +0800

    nromal publish

--- publish.sh
+++ publish.sh
@@ -2,10 +2,10 @@
 echo "build ..."
 
 # 移动 使用typra时产生的 assets图片到static/img 下面
-cp content/posts/assets/* static/img
-# 替换路径
-find ./content/ -type f -print0 | xargs -0 sed -i '' 's#../../static/#/#g'
-find ./content/ -type f -print0 | xargs -0 sed -i '' 's#assets/#/img/#g'
+# cp content/posts/assets/* static/img
+# # 替换路径
+# find ./content/ -type f -print0 | xargs -0 sed -i '' 's#../../static/#/#g'
+# find ./content/ -type f -print0 | xargs -0 sed -i '' 's#assets/#/img/#g'
 
 echo "git push ..."
 
